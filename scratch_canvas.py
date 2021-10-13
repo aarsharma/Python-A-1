4fd78b8236f3214fb53ede304ca76c14007077d8
aarsharma <92184790+aarsharma@users.noreply.github.com>
2021-10-14 00:53:47 +0530

    Add files via upload

---
+++ scratch_canvas.py
@@ -0,0 +1,27 @@
+import turtle
+
+turtle.pensize(4)
+
+turtle.color("blue")
+turtle.forward(150)
+turtle.left(120)
+
+turtle.color("red")
+turtle.setheading(-150)
+turtle.forward(150)
+
+turtle.color("pink")
+turtle.setheading(70)
+turtle.forward(150)
+
+turtle.color("yellow")
+turtle.setheading(290)
+turtle.forward(150)
+
+turtle.color("purple")
+turtle.setheading(148)
+turtle.forward(143)
+
+turtle.color("black")
+turtle.setheading(370)
+
